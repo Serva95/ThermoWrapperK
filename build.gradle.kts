3b30013f636c86d943667778019b28a719642bbf
Serva <43950391+Serva95@users.noreply.github.com>
2021-01-20 00:02:43 +0100

    spring and plugins version update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.4.1"
-	id("io.spring.dependency-management") version "1.0.10.RELEASE"
-	kotlin("jvm") version "1.4.10"
-	kotlin("plugin.spring") version "1.4.10"
-	kotlin("plugin.jpa") version "1.4.10"
+	id("org.springframework.boot") version "2.4.2"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	kotlin("jvm") version "1.4.21"
+	kotlin("plugin.spring") version "1.4.21"
+	kotlin("plugin.jpa") version "1.4.21"
 }
 
 group = "it.srv"
@@ -22,10 +22,10 @@ repositories {
 	mavenCentral()
 }
 
-var ktorVersion = "latest.release"
+var ktorVersion = "1.5.0"
 
 dependencies {
-	compileOnly("org.json:json:latest.release")
+	compileOnly("org.json:json:20201115")
 	implementation("io.ktor:ktor-client:$ktorVersion")
 	implementation("io.ktor:ktor-client-cio:$ktorVersion")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
